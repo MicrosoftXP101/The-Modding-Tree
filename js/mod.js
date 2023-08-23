74f5caccbbed6fc16e7e77c5632960ff7c90b46a
MicrosoftXP101 <130766396+MicrosoftXP101@users.noreply.github.com>
2023-08-23 19:51:42 -0400

    Update mod.js
    Nothing at all

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The JJT Tree",
+	id: "JJTDifficulty",
+	author: "Gideon Sundy",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
